8fb73f1ec152e90d98e972c3712b1b6c2c7d5eb6
Denis Kazantsev <denspa@mail.ru>
2021-11-11 20:53:57 +0600

    Заманена иконка приложения

--- app/src/main/java/com/kazantsev/coder/repo/api/model/UsersApi.kt
+++ app/src/main/java/com/kazantsev/coder/repo/api/model/UsersApi.kt
@@ -1,15 +1,30 @@
 package com.kazantsev.coder.repo.api.model
 
 import com.google.gson.annotations.SerializedName
+import com.kazantsev.coder.repo.db.model.UsersDb
 
 data class UsersApi(
-    @SerializedName("id") val id : String,
-    @SerializedName("avatarUrl") val avatarUrl : String,
-    @SerializedName("firstName") val firstName : String,
-    @SerializedName("lastName") val lastName : String,
-    @SerializedName("userTag") val userTag : String,
-    @SerializedName("department") val department : String,
-    @SerializedName("position") val position : String,
-    @SerializedName("birthday") val birthday : String,
-    @SerializedName("phone") val phone : String
+    @SerializedName("id") val id: String,
+    @SerializedName("avatarUrl") val avatarUrl: String,
+    @SerializedName("firstName") val firstName: String,
+    @SerializedName("lastName") val lastName: String,
+    @SerializedName("userTag") val userTag: String,
+    @SerializedName("department") val department: String,
+    @SerializedName("position") val position: String,
+    @SerializedName("birthday") val birthday: String,
+    @SerializedName("phone") val phone: String
 )
+
+fun UsersApi.toUsersDb(): UsersDb {
+    return UsersDb(
+        id = id,
+        avatarUrl = avatarUrl,
+        firstName = firstName,
+        lastName = lastName,
+        userTag = userTag,
+        department = department,
+        position = position,
+        birthday = birthday,
+        phone = phone
+    )
+}
